2079b1f5dab29bf6a3d1929aa5ac1ad6c076248f
mrjarvis698 <dudamajaykumar59@gmail.com>
2022-11-05 15:16:06 +0530

    update
    Signed-off-by: mrjarvis698 <dudamajaykumar59@gmail.com>

--- init.py
+++ init.py
@@ -2,6 +2,8 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options as ChromeOptions
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+import time
 
 def test_basic_options():
     service = ChromeService(executable_path=ChromeDriverManager().install())
@@ -17,7 +19,16 @@ def test_basic_options():
     chrome_options.add_argument("--log-level=3")
     chrome_options.add_argument("--output=/dev/null")
     driver = webdriver.Chrome(options=chrome_options, service=service)
+    driver.maximize_window()
     driver.get("https://cgqdc.in/")
+    driver.find_element(By.LINK_TEXT,"पंजीयन").click()
+    driver.find_element(By.ID,"mobile_btn").click()
+    driver.find_element(By.ID,"han").click()
+    time.sleep(5)
+    driver.find_element(By.ID,"mobile_no").click()
+    driver.find_element(By.ID,"mobile_no").send_keys("8103619402")
+    time.sleep(10)
+    driver.find_element(By.ID,"mobile_send_otp").click()
     driver.quit()
 
 test_basic_options()
