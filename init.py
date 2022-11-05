7e67496b5c88f2bacdaab152446509f025c022d5
mrjarvis698 <dudamajaykumar59@gmail.com>
2022-11-05 14:11:39 +0530

    add some exception parameters to ChromeOptions
    Signed-off-by: mrjarvis698 <dudamajaykumar59@gmail.com>

--- init.py
+++ init.py
@@ -3,12 +3,21 @@ from selenium.webdriver.chrome.options import Options as ChromeOptions
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 
-
 def test_basic_options():
     service = ChromeService(executable_path=ChromeDriverManager().install())
-    options = ChromeOptions()
-    driver = webdriver.Chrome(options=options, service=service)
-
+    chrome_options = ChromeOptions()
+    chrome_options.add_argument("--incognito")
+    chrome_options.add_argument("--disable-gpu")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-crash-reporter")
+    chrome_options.add_argument("--disable-extensions")
+    chrome_options.add_argument("--disable-in-process-stack-traces")
+    chrome_options.add_argument("--disable-logging")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--log-level=3")
+    chrome_options.add_argument("--output=/dev/null")
+    driver = webdriver.Chrome(options=chrome_options, service=service)
+    driver.get("https://cgqdc.in/")
     driver.quit()
 
 test_basic_options()
